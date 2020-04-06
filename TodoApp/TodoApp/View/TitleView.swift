2eca247d960535e4243903a56b4be3941351a3fa
delma <sdy2856@gmail.com>
2020-04-06 21:40:55 +0900

    feat :rocket: titleView의 tasksTitle 레이블 생성 #1

--- TodoApp/TodoApp/View/TitleView.swift
+++ TodoApp/TodoApp/View/TitleView.swift
@@ -13,12 +13,14 @@ class TitleView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(tasksCount)
+        self.addSubview(tasksTitle)
         setConstraints()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         self.addSubview(tasksCount)
+        self.addSubview(tasksTitle)
         setConstraints()
     }
 
@@ -33,12 +35,25 @@ class TitleView: UIView {
         return label
     }()
     
+    private var tasksTitle: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "to do"
+        return label
+    }()
+    
     func setConstraints() {
         tasksCount.translatesAutoresizingMaskIntoConstraints = false
         tasksCount.topAnchor.constraint(equalTo: self.topAnchor, constant: 15).isActive = true
         tasksCount.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
         tasksCount.widthAnchor.constraint(equalToConstant: 28).isActive = true
         tasksCount.heightAnchor.constraint(equalToConstant: 28).isActive = true
+        
+        tasksTitle.translatesAutoresizingMaskIntoConstraints = false
+        tasksTitle.topAnchor.constraint(equalTo: self.topAnchor, constant: 15).isActive = true
+        tasksTitle.leadingAnchor.constraint(equalTo: tasksCount.trailingAnchor, constant: 15).isActive = true
+        
     }
     
     func setTasksCount() {
