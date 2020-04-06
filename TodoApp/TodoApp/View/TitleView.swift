9b76a90dd04813ae196611bf045a00cfbdc82cdb
delma <sdy2856@gmail.com>
2020-04-06 21:36:00 +0900

    feat :rocket: titleView의 tasksCounts 레이블 생성

--- TodoApp/TodoApp/View/TitleView.swift
+++ TodoApp/TodoApp/View/TitleView.swift
@@ -12,11 +12,36 @@ class TitleView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.addSubview(tasksCount)
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        self.addSubview(tasksCount)
+        setConstraints()
     }
 
+    private var tasksCount: UILabel = {
+       let label = UILabel()
+        label.textColor = .black
+        label.backgroundColor = .white
+        label.clipsToBounds = true
+        label.layer.cornerRadius = label.font.pointSize * 0.80
+        label.textAlignment = .center
+        label.text = "22"
+        return label
+    }()
     
+    func setConstraints() {
+        tasksCount.translatesAutoresizingMaskIntoConstraints = false
+        tasksCount.topAnchor.constraint(equalTo: self.topAnchor, constant: 15).isActive = true
+        tasksCount.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        tasksCount.widthAnchor.constraint(equalToConstant: 28).isActive = true
+        tasksCount.heightAnchor.constraint(equalToConstant: 28).isActive = true
+    }
+    
+    func setTasksCount() {
+        
+    }
 }
