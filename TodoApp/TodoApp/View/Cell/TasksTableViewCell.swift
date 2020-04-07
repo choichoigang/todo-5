33044fc3c21394bcfee4dd0a3dddc801694d76cb
delma <sdy2856@gmail.com>
2020-04-07 17:55:32 +0900

    refactor :rocket: tasksTableViewCell의 내부 속성 컨스트레인트 수정 #1

--- TodoApp/TodoApp/View/Cell/TasksTableViewCell.swift
+++ TodoApp/TodoApp/View/Cell/TasksTableViewCell.swift
@@ -12,13 +12,13 @@ class TasksTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .default, reuseIdentifier: "TasksTableViewCell")
-        self.addSubview(labelsStack)
+        self.contentView.addSubview(stackWithButton)
         setConstraints()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.addSubview(labelsStack)
+        self.contentView.addSubview(stackWithButton)
         setConstraints()
     }
     
@@ -29,7 +29,7 @@ class TasksTableViewCell: UITableViewCell {
     private var taskTitle: UILabel = {
         let label = UILabel()
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 18)
+        label.font = UIFont.boldSystemFont(ofSize: 18)
         label.text = "UI 만들기"
         return label
     }()
@@ -63,12 +63,32 @@ class TasksTableViewCell: UITableViewCell {
         return stackView
     }()
     
+    private var dragButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "equal.square"), for: .normal)
+        button.tintColor = .gray
+        return button
+    }()
+    
+    lazy var stackWithButton: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 3
+        stackView.distribution = .fillProportionally
+        
+        stackView.addArrangedSubview(dragButton)
+        stackView.addArrangedSubview(labelsStack)
+        return stackView
+    }()
+    
     func setConstraints() {
-        labelsStack.translatesAutoresizingMaskIntoConstraints = false
-        labelsStack.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
-        labelsStack.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0).isActive = true
-        labelsStack.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0).isActive = true
-        labelsStack.heightAnchor.constraint(equalTo: self.heightAnchor, constant: -10).isActive = true
+        stackWithButton.translatesAutoresizingMaskIntoConstraints = false
+        stackWithButton.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 0).isActive = true
+        stackWithButton.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 0).isActive = true
+        stackWithButton.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: 0).isActive = true
+        stackWithButton.heightAnchor.constraint(equalTo: self.contentView.heightAnchor, constant: -10).isActive = true
+        stackWithButton.widthAnchor.constraint(equalTo: self.contentView.widthAnchor, constant: 0).isActive = true
     }
     
 }
