e178f348d78446c4ca823c3f5e87dc8eded69cf2
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-08 11:11:52 +0900

    chore :seedling: 기본 서비스 인터페이스 설정

---
+++ BE/src/main/java/com/codesquad/todo5/service/ActivityService.java
@@ -0,0 +1,10 @@
+package com.codesquad.todo5.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+}
