eb32346077b513de422098a3496430a3e7d8c57e
최장호 <hmchoi651@naver.com>
2020-04-08 22:06:34 +0900

    :rocket: feat drop 콜백 함수 추가

--- frontend/js/DragDropEvent.js
+++ frontend/js/DragDropEvent.js
@@ -5,3 +5,12 @@ const dragStartHandler = (event) => {
 const dragOverHandler = (event) => {
   event.preventDefault();
 };
+
+const dropHanlder = (event) => {
+  const getID = event.dataTransfer.getData("ID");
+  const dropArea = event.target.classList.contains("box");
+
+  if (dropArea) {
+    event.target.appendChild(document.getElementById(getID));
+  }
+};
