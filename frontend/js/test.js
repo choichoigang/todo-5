9720c8fa47a0265b6f2e01c028a683ac2d97e754
최장호 <hmchoi651@naver.com>
2020-04-10 01:16:55 +0900

    :checkered_flag: test 테스트 파일 구현

---
+++ frontend/js/test.js
@@ -0,0 +1,7 @@
+import DragDropEvent from "./DragDropEvent.js";
+import testEvent from "./clickEvent.js";
+import initTextareaEvent from "./textareaEvent.js";
+
+DragDropEvent();
+testEvent();
+initTextareaEvent();
