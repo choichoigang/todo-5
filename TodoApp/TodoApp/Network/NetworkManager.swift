21f9a6eb67415212ec80f70f6bb937e699044792
delma <sdy2856@gmail.com>
2020-04-09 12:08:54 +0900

    feat :rocket: network manager 추가

---
+++ TodoApp/TodoApp/Network/NetworkManager.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkManager.swift
+//  TodoApp
+//
+//  Created by delma on 2020/04/09.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkErrorCase: Error {
+    case InvalidURL
+    case DecodeError
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+struct NetworkManager {
+    enum EndPoints {
+        static let AllData = ""
+    }
+    
+    func getResource(url: URL, methodType: HTTPMethod, body: Data? = nil, completion: @escaping(Result<Any, NetworkErrorCase>) -> Void) {
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = methodType.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil else {
+                if let error = error as NSError?, error.domain == NSURLErrorDomain { completion(.failure(.InvalidURL)) }
+                return
+            }
+            completion(.success(data))
+        }.resume()
+    }
+}
