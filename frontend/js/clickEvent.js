b51d98553bf6a5fd9099789aea755284c20f7265
최장호 <hmchoi651@naver.com>
2020-04-10 01:02:16 +0900

    :rocket: feat activatingHandler 함수
    activatingHandler : plus 버튼을 누르거나 Cancel 버튼을 누를때 입력창 활성화 비활성화

--- frontend/js/clickEvent.js
+++ frontend/js/clickEvent.js
@@ -1,3 +1,13 @@
 import { createDOM, cerateColumnDom } from "../options/DOM.js";
 import classNameObj from "../options/columnClassName.js";
 import itemTemplate from "../template/template.js";
+
+const activatingHandler = (addBoxDom) => {
+  const displayValue = addBoxDom.style.display;
+
+  if (!displayValue) {
+    addBoxDom.style.display = "flex";
+  } else {
+    addBoxDom.style.display = "";
+  }
+};
