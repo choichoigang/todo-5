f05d7b0290d9cf4c84b36859e119294272e38de8
최장호 <hmchoi651@naver.com>
2020-04-10 00:52:15 +0900

    :rocket: column에 포함된 하위 속성 DOM 호출 함수

--- frontend/options/DOM.js
+++ frontend/options/DOM.js
@@ -9,3 +9,14 @@ const DOM = {
 const cerateColumnDom = (columnName) => {
   return document.querySelector(`#${columnName}`);
 };
+
+const createDOM = (columnName) => {
+  return {
+    columnName: `${columnName}`,
+    column: document.querySelector(`#${columnName}`),
+    addBox: document.querySelector(`#${columnName}_add_area`),
+    textArea: document.querySelector(`#${columnName}_add_area textarea`),
+    addButton: document.querySelector(`.${columnName}_add_btn`),
+    cancelButton: document.querySelector(`.${columnName}_cancel_btn`),
+  };
+};
