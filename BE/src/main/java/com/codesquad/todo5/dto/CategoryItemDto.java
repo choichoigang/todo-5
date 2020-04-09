a908867be1002f9062af083f05ad82a797de8683
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-09 17:48:44 +0900

    feat :wrench: DTO 객체 인터페이스 생성

--- BE/src/main/java/com/codesquad/todo5/dto/CategoryItemDto.java
+++ BE/src/main/java/com/codesquad/todo5/dto/CategoryItemDto.java
@@ -1,5 +1,20 @@
 package com.codesquad.todo5.dto;
 
-public class CategoryDto {
+import com.codesquad.todo5.domain.CategoryItem;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryItemDto {
+  private Long id;
+  private String name;
+
+  public CategoryItemDto(CategoryItem item) {
+    this.id = item.getId();
+    this.name = item.getName();
+  }
 }
---
+++ BE/src/main/java/com/codesquad/todo5/dto/CategoryItemListDto.java
@@ -0,0 +1,15 @@
+package com.codesquad.todo5.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryItemListDto {
+  private Object data;
+  private Object status;
+}
