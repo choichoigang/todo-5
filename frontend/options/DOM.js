83ba5e0844a4fbf928076b24792c65ac5ae75de1
최장호 <hmchoi651@naver.com>
2020-04-10 00:50:00 +0900

    :page_facing_up: docs column 을 함수화 하기 위해 일부 값 삭제

--- frontend/options/DOM.js
+++ frontend/options/DOM.js
@@ -2,9 +2,6 @@ const DOM = {
   dropTarget: document.querySelector(".wrapper"),
 
   todoBox: document.querySelector("#todo"),
-  todoAddBox: document.querySelector("#todo_add_area"),
-  todoTextArea: document.querySelector("#todo_add_area textarea"),
-  todoAddButton: document.querySelector(".todo_add_btn"),
+  doingBox: document.querySelector("#doing"),
+  doneBox: document.querySelector("#done"),
 };
-
-export default DOM;
