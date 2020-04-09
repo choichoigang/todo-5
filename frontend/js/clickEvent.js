a884d4b7401a1add71d8c3d9a851d02781b393c1
최장호 <hmchoi651@naver.com>
2020-04-10 01:06:19 +0900

    :rocket: feat ClickEventHandler function
    ClickEventHandler : Click Event 최종 콜백 함수 , 내부에서 각 분기마다 이벤트를 처리

--- frontend/js/clickEvent.js
+++ frontend/js/clickEvent.js
@@ -2,6 +2,17 @@ import { createDOM, cerateColumnDom } from "../options/DOM.js";
 import classNameObj from "../options/columnClassName.js";
 import itemTemplate from "../template/template.js";
 
+const ClickEventHandler = (event, className, DOM) => {
+  const targetClassName = event.target.className;
+  if (targetClassName === className.plusButton) {
+    activatingHandler(DOM.addBox);
+  } else if (targetClassName === className.addButton) {
+    DOM.column.innerHTML += addBtnHandler(DOM.textArea);
+  } else if (targetClassName === className.cancelButton) {
+    activatingHandler(DOM.addBox);
+  }
+};
+
 const activatingHandler = (addBoxDom) => {
   const displayValue = addBoxDom.style.display;
 
