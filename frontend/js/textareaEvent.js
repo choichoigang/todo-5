f362c96f0a70b17dca7f624dcd9ddbb8a6deb76b
최장호 <hmchoi651@naver.com>
2020-04-10 01:15:10 +0900

    :rocket: feat textareaHandler function
    textareaHandler : textarea의 입력 상태에 따라서 Add 버튼을 활성화 시키는 함수

--- frontend/js/textareaEvent.js
+++ frontend/js/textareaEvent.js
@@ -1,1 +1,12 @@
 import { createTextareaDOM } from "../options/DOM.js";
+
+const textareaHandler = (event, todoAddButton) => {
+  const textareaValue = event.target.value;
+
+  if (textareaValue === "") {
+    todoAddButton.disabled = true;
+    return;
+  } else {
+    todoAddButton.disabled = false;
+  }
+};
