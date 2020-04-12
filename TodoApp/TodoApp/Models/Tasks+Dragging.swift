b4258212bc816473bacb4f2ec6660ac951763756
delma <sdy2856@gmail.com>
2020-04-12 15:19:43 +0900

    feat :rocket: drag 위한 메서드 추가 #53

---
+++ TodoApp/TodoApp/Models/Tasks+Dragging.swift
@@ -0,0 +1,33 @@
+//
+//  Tasks+Dragging.swift
+//  TodoApp
+//
+//  Created by delma on 2020/04/12.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import MobileCoreServices
+import UIKit
+
+extension Category {
+    func canHandle(_ session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSString.self)
+    }
+    
+    func dragItems(for indexPath: IndexPath) -> [UIDragItem] {
+        let task = tasks[indexPath.row]
+        let encoder = JSONEncoder()
+        let data = try? encoder.encode(task)
+        
+        let itemProvider = NSItemProvider()
+        
+        itemProvider.registerDataRepresentation(forTypeIdentifier: kUTTypePlainText as String, visibility: .all) { completion in
+            completion(data, nil)
+            return nil
+        }
+        
+        return [
+            UIDragItem(itemProvider: itemProvider)
+        ]
+    }
+}
