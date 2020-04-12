a706ab64bd862c14cca161831707d680d3dbe339
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-12 09:48:07 +0900

    chore :construction: UserService 서비스 기본 인터페이스 설계

---
+++ BE/src/main/java/com/codesquad/todo5/service/UserService.java
@@ -0,0 +1,23 @@
+package com.codesquad.todo5.service;
+
+import com.codesquad.todo5.domain.user.User;
+import com.codesquad.todo5.domain.user.UserRepository;
+import com.codesquad.todo5.exception.ResourceNotFoundException;
+import java.util.Optional;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Transactional(readOnly = true)
+  public User getUserByName(String name) {
+    return userRepository.findByName(name).orElseThrow(() -> new ResourceNotFoundException());
+  }
+}
