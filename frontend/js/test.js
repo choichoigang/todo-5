444ecc70204ba5739fb4f0fa1ef4156a46cdb5e2
최장호 <hmchoi651@naver.com>
2020-04-13 14:35:25 +0900

    :checkered_flag: test modal event test code 추가

--- frontend/js/test.js
+++ frontend/js/test.js
@@ -1,7 +1,9 @@
 import DragDropEvent from "./DragDropEvent.js";
 import testEvent from "./clickEvent.js";
 import initTextareaEvent from "./textareaEvent.js";
+import testing from "./render.js";
 
 DragDropEvent();
 testEvent();
 initTextareaEvent();
+testing();
