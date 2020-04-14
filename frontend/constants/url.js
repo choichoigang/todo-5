943384c8a2b64835d823519a4bf90d6d92dd6153
최장호 <hmchoi651@naver.com>
2020-04-15 01:10:27 +0900

    :page_facing_up: docs edit시 요청 url 형식 추가 및 delete 인자 이름 수정

--- frontend/constants/url.js
+++ frontend/constants/url.js
@@ -3,8 +3,11 @@ const TODO_URL = {
   DOING_LIST: "http://13.209.180.92:8080/api/category/2/all",
   DONE_LIST: "http://13.209.180.92:8080/api/category/3/all",
   ADD: "http://13.209.180.92:8080/api/task/add",
-  DELETE: (taskID) => {
-    return `http://13.209.180.92:8080/api/task/${taskID}/delete`;
+  DELETE: (taskId) => {
+    return `http://13.209.180.92:8080/api/task/${taskId}/delete`;
+  },
+  EDIT: (taskId) => {
+    return `http://13.209.180.92:8080/api/task/${taskId}/edit`;
   },
 };
 
