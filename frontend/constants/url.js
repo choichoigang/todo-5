8cb86e534e5514e1e84a2d31aca99e5bc25c5860
최장호 <hmchoi651@naver.com>
2020-04-14 20:23:47 +0900

    :rocekt: feat add , delete url 추가

--- frontend/constants/url.js
+++ frontend/constants/url.js
@@ -2,6 +2,10 @@ const TODO_URL = {
   TODO_LIST: "http://13.209.180.92:8080/api/category/1/all",
   DOING_LIST: "http://13.209.180.92:8080/api/category/2/all",
   DONE_LIST: "http://13.209.180.92:8080/api/category/3/all",
+  ADD: "http://13.209.180.92:8080/api/task/add",
+  DELETE: (taskID) => {
+    return `http://13.209.180.92:8080/api/task/${taskID}/delete`;
+  },
 };
 
 export default TODO_URL;
