af88c0b4c06590435f8703873711fc2e0267db2f
최장호 <hmchoi651@naver.com>
2020-04-15 18:10:48 +0900

    :page_facing_up: docs import문 수정 및 renderList 함수에 if문 분기처리 추가

--- frontend/js/render.js
+++ frontend/js/render.js
@@ -1,24 +1,30 @@
-import { itemTemplate, renderItem } from "../template/template.js";
+import { taskTemplate } from "../template/template.js";
 import { commonDOM } from "../options/DOM.js";
 import URL from "../constants/url.js";
-import { fetchTodoList } from "./fetch.js";
+import { fetchTodoList } from "./httpRequest.js";
 
 const registerTodoList = (className) => {
   if (className === "todo") {
-    fetchTodoList(URL.TODO_LIST, commonDOM.todoBox, className);
+    fetchTodoList(URL.TODO_LIST, commonDOM.todoTaskList, className);
   } else if (className === "doing") {
-    fetchTodoList(URL.DOING_LIST, commonDOM.doingBox, className);
+    fetchTodoList(URL.DOING_LIST, commonDOM.doingTaskList, className);
   } else if (className === "done") {
-    fetchTodoList(URL.DONE_LIST, commonDOM.doneBox, className);
+    fetchTodoList(URL.DONE_LIST, commonDOM.doneTaskList, className);
   }
 };
 
 export const renderList = (columnDom, dataList, className) => {
   dataList.forEach((el) => {
-    columnDom.innerHTML += renderItem(el.title, className, el.id);
+    if (el.deleted === true) {
+      return;
+    } else {
+      columnDom.innerHTML += taskTemplate(el.title, el.id, className);
+    }
   });
 };
 
 export const initRenderTodoList = () => {
   registerTodoList("todo");
+  registerTodoList("doing");
+  registerTodoList("done");
 };
