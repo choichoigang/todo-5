954b5b19dd29e05c6244ea2c71afc9f3aad971fb
delma <sdy2856@gmail.com>
2020-04-15 16:35:00 +0900

    refactor :recycle: method 분리 #79

--- TodoApp/TodoApp/ViewControllers/MainViewController.swift
+++ TodoApp/TodoApp/ViewControllers/MainViewController.swift
@@ -29,7 +29,6 @@ class ViewController: UIViewController {
         configureViews()
         addSubViews()
         setConstraints()
-        
         requestAllData()
         addObserver()
     }
@@ -52,45 +51,27 @@ class ViewController: UIViewController {
         self.view.addSubview(thirdView!)
     }
     
-    func setConstraints() {
-        firstView?.translatesAutoresizingMaskIntoConstraints = false
-        firstView?.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 0).isActive = true
-        firstView?.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
-        firstView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
-        firstView?.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.33, constant: 0).isActive = true
-        
-        secondView?.translatesAutoresizingMaskIntoConstraints = false
-        secondView?.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 0).isActive = true
-        secondView?.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
-        secondView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
-        secondView?.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.33, constant: 0).isActive = true
-        
-        thirdView?.translatesAutoresizingMaskIntoConstraints = false
-        thirdView?.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 0).isActive = true
-        thirdView?.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
-        thirdView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
-        thirdView?.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.33, constant: 0).isActive = true
+    private func requestAllData() {
+        networkManager.getResource(url: EndPoints.AllCategories!, methodType: .get, dataType: AllData.self) { self.branchAllData(result: $0) }
     }
     
-    func requestAllData() {
-        networkManager.getResource(url: EndPoints.AllCategories!, methodType: .get, dataType: AllData.self) { result in
-            switch result {
-            case .success(let anyData):
-                self.allData = anyData as? AllData
-                guard let allData = self.allData else { return }
-                DispatchQueue.main.async {
-                    for index in 0..<self.controllers.count {
-                        self.controllers[index].category = allData.data[index]
-                    }
+    private func branchAllData(result: Result<Any, NetworkErrorCase>) {
+        switch result {
+        case .success(let anyData):
+            self.allData = anyData as? AllData
+            guard let allData = self.allData else { return }
+            DispatchQueue.main.async {
+                for index in 0..<self.controllers.count {
+                    self.controllers[index].category = allData.data[index]
                 }
-            case .failure(let error):
-                //네트워크 오류 알림 알럿창 생성
-                print(error.localizedDescription)
             }
+        case .failure(let error):
+            //네트워크 오류 알림 알럿창 생성
+            print(error.localizedDescription)
         }
     }
     
-    func addObserver() {
+    private func addObserver() {
         NotificationCenter.default.addObserver(self, selector: #selector(requestOneCategory(_:)), name: .addNewCard, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(updateData(_:)), name: .updateCount, object: nil)
     }
@@ -114,6 +95,10 @@ class ViewController: UIViewController {
     @objc func updateData(_ notification: Notification) {
         guard let updateInfo = notification.userInfo?["updateInfo"] as? (count: Int, categoryID: Int, taskID: Int) else { return }
         requestDelete(taskID: updateInfo.taskID)
+        updateTasksCount(updateInfo: updateInfo)
+    }
+    
+    private func updateTasksCount(updateInfo: (count: Int, categoryID: Int, taskID: Int)) {
         let targetController = controllers.filter { $0.category?.id == updateInfo.categoryID }.first
         guard let controller = targetController else { return }
         controller.titleView.setTasksCount(count: updateInfo.count)
@@ -124,6 +109,27 @@ class ViewController: UIViewController {
         let url = URL(string: urlString)
         networkManager.getResource(url: url!, methodType: .post, dataType: RequestBody.self) { _ in }
     }
+    
+    private func setConstraints() {
+        firstView?.translatesAutoresizingMaskIntoConstraints = false
+        firstView?.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 0).isActive = true
+        firstView?.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0).isActive = true
+        firstView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
+        firstView?.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.33, constant: 0).isActive = true
+        
+        secondView?.translatesAutoresizingMaskIntoConstraints = false
+        secondView?.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 0).isActive = true
+        secondView?.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0).isActive = true
+        secondView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
+        secondView?.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.33, constant: 0).isActive = true
+        
+        thirdView?.translatesAutoresizingMaskIntoConstraints = false
+        thirdView?.topAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: 0).isActive = true
+        thirdView?.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0).isActive = true
+        thirdView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
+        thirdView?.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.33, constant: 0).isActive = true
+    }
+
 }
 
 extension Notification.Name {
