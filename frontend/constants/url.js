c94b2f25937f42e38649c6e171a422b8c9b48073
최장호 <hmchoi651@naver.com>
2020-04-17 00:23:17 +0900

    :rocket: feat User Activity URL 추가

--- frontend/constants/url.js
+++ frontend/constants/url.js
@@ -3,6 +3,7 @@ const TODO_URL = {
   DOING_LIST: "http://13.209.180.92:8080/api/category/2/all",
   DONE_LIST: "http://13.209.180.92:8080/api/category/3/all",
   ADD: "http://13.209.180.92:8080/api/task/add",
+  ACTIVITY: "http://13.209.180.92:8080/api/user/jypthemiracle/activity",
   DELETE: (taskId) => {
     return `http://13.209.180.92:8080/api/task/${taskId}/delete`;
   },
