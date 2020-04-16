bb9f957fd9157abb6ae080ac6ae82215d1a0ed05
최장호 <hmchoi651@naver.com>
2020-04-16 13:44:19 +0900

    :rocket: feat action 관련 template 추가

--- frontend/template/template.js
+++ frontend/template/template.js
@@ -15,3 +15,68 @@ export const renderTaskTemplate = (inputValue, id, className) => {
     </div>
   </div>`;
 };
+
+export const renderActionAdd = (taskTitle, columnName) => {
+  return `<div class="log">
+  <div class="profile">
+    <img
+      src="https://images.unsplash.com/photo-1513333420772-7b64ad15ca96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80"
+    />
+  </div>
+  <div class="content">
+    <span class="user_id">@hoi </span>
+    <span class="action">added </span>
+    <span class="task_title">${taskTitle} </span>
+    <span class="column_name">to ${columnName}</span>
+  </div>
+</div>`;
+};
+
+export const renderActionDelete = (taskTitle, columnName) => {
+  return `<div class="log">
+  <div class="profile">
+    <img
+      src="https://images.unsplash.com/photo-1513333420772-7b64ad15ca96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80"
+    />
+  </div>
+  <div class="content">
+    <span class="user_id">@hoi </span>
+    <span class="action">delete </span>
+    <span class="task_title">${taskTitle} </span>
+    <span class="column_name">${columnName}</span>
+  </div>
+</div>`;
+};
+
+export const renderActionEdit = (taskTitle, columnName) => {
+  return `<div class="log">
+  <div class="profile">
+    <img
+      src="https://images.unsplash.com/photo-1513333420772-7b64ad15ca96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80"
+    />
+  </div>
+  <div class="content">
+    <span class="user_id">@hoi </span>
+    <span class="action">updated </span>
+    <span class="task_title">${taskTitle} </span>
+    <span class="column_name">${columnName}</span>
+  </div>
+</div>`;
+};
+
+export const renderActionMove = (taskTitle, fromColumnName, toColumnName) => {
+  return `<div class="log">
+  <div class="profile">
+    <img
+      src="https://images.unsplash.com/photo-1513333420772-7b64ad15ca96?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80"
+    />
+  </div>
+  <div class="content">
+    <span class="user_id">@hoi </span>
+    <span class="action">moved </span>
+    <span class="task_title">${taskTitle} </span>
+    <span class="from_column">from ${fromColumnName} </span>
+    <span class="to_column">to ${toColumnName}</span>
+  </div>
+</div>`;
+};
