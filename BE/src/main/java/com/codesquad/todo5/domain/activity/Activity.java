5f0d50920abe15cbaff77c8c888f4b40784199f2
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-16 19:37:03 +0900

    chore :construction: Activity가 불필요한 필드값을 갖지 않도록 수정

--- BE/src/main/java/com/codesquad/todo5/domain/activity/Activity.java
+++ BE/src/main/java/com/codesquad/todo5/domain/activity/Activity.java
@@ -3,6 +3,8 @@ package com.codesquad.todo5.domain.activity;
 import com.codesquad.todo5.domain.task.Task;
 import com.codesquad.todo5.domain.user.User;
 import java.sql.Timestamp;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,24 +28,22 @@ public class Activity {
   private String userName;
   @Column("user")
   private Long userId;
-  private Timestamp createdDate;
+  private String createdDate;
   private Target target;
   private Action action;
   private Long categoryFrom;
   private Long categoryTo;
-  @Column("previous_target_name")
-  private String previousTargetName;
-  @Column("next_target_name")
-  private String nextTargetName;
+  @Column("target_title")
+  private String targetTitle;
 
   private Activity(String userName, Action action, Target entity) {
+
     this.userName = userName;
     this.action = action;
     this.target = entity;
 //  TODO 이거 서버에서 해줘야 하는데 DEFAULT current_timestamp 선언 해줬는데 NULL로 찍혀서 임시방편 처리하였음
 //  리팩토링 대상임!!
-    Date date = new Date();
-    this.createdDate = new Timestamp(date.getTime());
+    this.createdDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
   }
 
   public static Activity create(String userName, Action action, Target target) {
