53d13ab0663772895d6a065c9304b59f457e44ca
최장호 <hmchoi651@naver.com>
2020-04-16 18:06:56 +0900

    :rocket: feat Action render시에 필요한 option 값들 추가

---
+++ frontend/options/actionOption.js
@@ -0,0 +1,31 @@
+const addActionOption = {
+  actionName: "ADD",
+  taskTitle: null,
+  categoryTo: null,
+};
+
+const removeActionOption = {
+  actionName: "REMOVE",
+  taskTitle: null,
+  categoryTo: null,
+};
+
+const updateActionOption = {
+  actionName: "UPDATE",
+  taskTitle: null,
+  categoryTo: null,
+};
+
+const moveActionOption = {
+  actionName: "MOVE",
+  taskTitle: null,
+  categoryTo: null,
+  categoryFrom: null,
+};
+
+export {
+  addActionOption,
+  removeActionOption,
+  updateActionOption,
+  moveActionOption,
+};
