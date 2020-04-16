f3ab2379eb9e90f6e966da4b07e482b83d919d1c
Jin Hyung Park <hophfg@yahoo.co.kr>
2020-04-16 17:37:06 +0900

    chore :wrench: activity 객체 필드값 변경

--- BE/src/main/java/com/codesquad/todo5/domain/activity/Activity.java
+++ BE/src/main/java/com/codesquad/todo5/domain/activity/Activity.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
@@ -21,21 +22,28 @@ public class Activity {
   // 컬럼으로 인식하여 에러 발생
 //  private User user;
 //  private Task task;
+  @Column("user_name")
   private String userName;
+  @Column("user")
+  private Long userId;
   private Timestamp createdDate;
   private Target target;
   private Action action;
   private Long categoryFrom;
   private Long categoryTo;
+  @Column("previous_target_name")
+  private String previousTargetName;
+  @Column("next_target_name")
+  private String nextTargetName;
 
   private Activity(String userName, Action action, Target entity) {
     this.userName = userName;
     this.action = action;
     this.target = entity;
-//    //TODO 이거 서버에서 해줘야 하는데 DEFAULT current_timestamp 선언 해줬는데 NULL로 찍혀서 임시방편 처리하였음
-//    //리팩토링 대상임!!
-//    Date date = new Date();
-//    this.createdDate = new Timestamp(date.getTime());
+//  TODO 이거 서버에서 해줘야 하는데 DEFAULT current_timestamp 선언 해줬는데 NULL로 찍혀서 임시방편 처리하였음
+//  리팩토링 대상임!!
+    Date date = new Date();
+    this.createdDate = new Timestamp(date.getTime());
   }
 
   public static Activity create(String userName, Action action, Target target) {
