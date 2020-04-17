dd024b15f7b93a03bf825bae46a96efe9c115910
최장호 <hmchoi651@naver.com>
2020-04-17 14:17:27 +0900

    :rocket: feat LogIn 이벤트 등록함수 추가

--- frontend/js/eventRegister.js
+++ frontend/js/eventRegister.js
@@ -2,8 +2,10 @@ import DragDropEvent from "./event/DragDropEvent.js";
 import initClickEvent from "./event/clickEvent.js";
 import initTextareaEvent from "./event/textareaEvent.js";
 import { initRenderTodoList } from "./render/render.js";
+import { registerLogInEvent } from "./event/logInEvent.js";
 
 initClickEvent();
 initTextareaEvent();
 DragDropEvent();
 initRenderTodoList();
+registerLogInEvent();
