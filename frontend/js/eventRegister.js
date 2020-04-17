522dc4ef1d89d0ed46785087dd28261d901dba96
최장호 <hmchoi651@naver.com>
2020-04-17 14:27:44 +0900

    :page_facing_up: docs Webpack 환경에 따라서 html , js 구조 변경

--- frontend/js/eventRegister.js
+++ frontend/js/eventRegister.js
@@ -4,6 +4,10 @@ import initTextareaEvent from "./event/textareaEvent.js";
 import { initRenderTodoList } from "./render/render.js";
 import { registerLogInEvent } from "./event/logInEvent.js";
 
+import resetCSS from "../css/reset.css";
+import mainCSS from "../css/main.css";
+import listCSS from "../css/list.css";
+
 initClickEvent();
 initTextareaEvent();
 DragDropEvent();
